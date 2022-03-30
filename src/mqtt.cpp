e450482617b344c52f7eb91577a04988e2f60825
Jonathan Mataloni <jo.mataloni@gmail.com>
2022-03-31 01:25:55 +0200

    Fix ptr refs

--- src/mqtt.cpp
+++ src/mqtt.cpp
@@ -6,33 +6,34 @@
 #include "config.h"
 #include "utils.h"
 
-void InitMQTT(PubSubClient &mqttClient)
+void init_mqtt(PubSubClient *mqttClient)
 {
-  mqttClient.setServer(MQTT_SERVER, 1883);
+  (*mqttClient).setServer(MQTT_SERVER, 1883);
 }
 
-void MqttConnectionLoop(PubSubClient &mqttClient)
+void mqtt_connection_task(PubSubClient *mqttClient )
 {
 
-  if (!mqttClient.connected())
-  {
-    // TODO remove blocking code
-    while (!mqttClient.connected())
+    if (!(*mqttClient).connected())
     {
-      String clientId = SAT_NAME "-client-" + String(random(0xffff), HEX);
+      // TODO remove blocking code
+      while (!(*mqttClient).connected())
+      {
+        String clientId = SAT_NAME "-client-" + String(random(0xffff), HEX);
 
-      if (mqttClient.connect(clientId.c_str()))
-      {
-        log(SAT_NAME " connected to MQTT broker " MQTT_SERVER " on port " + String(MQTT_PORT) + " as " + clientId);
-        mqttClient.subscribe(SYSTEM_NAME "/#");
-      }
-      else
-      {
-        log("cannot connect to MQTT broker. Status:" + String(mqttClient.state()) + ". Retring...");
-        delay(RECONNECTION_TIME);
+        if ((*mqttClient).connect(clientId.c_str()))
+        {
+          log(SAT_NAME " connected to MQTT broker " MQTT_SERVER " on port " + String(MQTT_PORT) + " as " + clientId);
+          (*mqttClient).subscribe(SYSTEM_NAME "/#");
+        }
+        else
+        {
+          log("cannot connect to MQTT broker. Status:" + String((*mqttClient).state()) + ". Retring...");
+          delay(RECONNECTION_TIME);
+        }
       }
     }
-  }
 
-  mqttClient.loop();
+    (*mqttClient).loop();
+  
 }
--- src/ota.cpp
+++ src/ota.cpp
@@ -7,7 +7,7 @@
 unsigned long lastMsg = 0;
 char msg[MSG_BUFFER_SIZE];
 
-void InitOTA(AsyncElegantOtaClass &ota, AsyncWebServer &webServer)
+void init_ota(AsyncElegantOtaClass *ota, AsyncWebServer *webServer)
 {
-  ota.begin(&webServer);
+  (*ota).begin(webServer);
 }
